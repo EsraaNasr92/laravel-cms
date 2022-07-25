20381f2ccff5c4fa0e244fda2f8ef05d8b1ba4e6
EsraaNasr92 <esraanasr706@gmail.com>
2022-07-25 18:33:47 +0200

    category issue

--- resources/views/admin/pcategory/index.blade.php
+++ resources/views/admin/pcategory/index.blade.php
@@ -127,69 +127,77 @@
                     </div>
                     <!-- END: Pagination -->
                 </div>
-              <!-- BEGIN: Delete Confirmation Modal -->
-                <div id="delete-confirmation-modal" class="modal" tabindex="-1" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-body p-0">
-                                <div class="p-5 text-center">
-                                    <i data-lucide="x-circle" class="w-16 h-16 text-danger mx-auto mt-3"></i> 
-                                    <div class="text-3xl mt-5">Are you sure?</div>
-                                    <div class="text-slate-500 mt-2">
-                                        Do you really want to delete these records? 
-                                        <br>
-                                        This process cannot be undone.
-                                    </div>
-                                </div>
-                                <div class="px-5 pb-8 text-center">
-                               
-                                  <form action="{{ route('pcategory.destroy', $category->id) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="button" data-tw-dismiss="modal" class="btn btn-outline-secondary w-24 mr-1">Cancel</button>
-                                    <button type="submit" class="btn btn-danger w-24">Delete</button>
-                                  </form>
+
+                @foreach ($categories as $category)   
+                  <!-- BEGIN: Delete Confirmation Modal -->
+                  <div id="delete-confirmation-modal" class="modal" tabindex="-1" aria-hidden="true">
+                      <div class="modal-dialog">
+                          <div class="modal-content">
+                              <div class="modal-body p-0">
+                                  <div class="p-5 text-center">
+                                      <i data-lucide="x-circle" class="w-16 h-16 text-danger mx-auto mt-3"></i> 
+                                      <div class="text-3xl mt-5">Are you sure?</div>
+                                      <div class="text-slate-500 mt-2">
+                                          Do you really want to delete these records? 
+                                          <br>
+                                          This process cannot be undone.
+                                      </div>
+                                  </div>
+                                  <div class="px-5 pb-8 text-center">
                                 
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <!-- END: Delete Confirmation Modal -->
+                                    <form action="{{ route('pcategory.destroy', $category->id) }}" method="POST">
+                                      @csrf
+                                      @method('DELETE')
+                                      <button type="button" data-tw-dismiss="modal" class="btn btn-outline-secondary w-24 mr-1">Cancel</button>
+                                      <button type="submit" class="btn btn-danger w-24">Delete</button>
+                                    </form>
+                                  
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+                  <!-- END: Delete Confirmation Modal -->
 
 
-
-              <!-- BEGIN: Delete Child Confirmation Modal -->
-              <div id="delete-confirmation-modal-child" class="modal" tabindex="-1" aria-hidden="true">
-                    <div class="modal-dialog">
-                        <div class="modal-content">
-                            <div class="modal-body p-0">
-                                <div class="p-5 text-center">
-                                    <i data-lucide="x-circle" class="w-16 h-16 text-danger mx-auto mt-3"></i> 
-                                    <div class="text-3xl mt-5">Are you sure?</div>
-                                    <div class="text-slate-500 mt-2">
-                                        Do you really want to delete these records? 
-                                        <br>
-                                        This process cannot be undone.
+              @if ($category->children)
+                @foreach ($category->children as $child)
+                  <!-- BEGIN: Delete Child Confirmation Modal -->
+                  <div id="delete-confirmation-modal-child" class="modal" tabindex="-1" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-body p-0">
+                                    <div class="p-5 text-center">
+                                        <i data-lucide="x-circle" class="w-16 h-16 text-danger mx-auto mt-3"></i> 
+                                        <div class="text-3xl mt-5">Are you sure?</div>
+                                        <div class="text-slate-500 mt-2">
+                                            Do you really want to delete these records? 
+                                            <br>
+                                            This process cannot be undone.
+                                        </div>
+                                    </div>
+                                    <div class="px-5 pb-8 text-center">
+                                  
+                                  
+                                      <form action="{{ route('pcategory.destroy', $child->id) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="button" data-tw-dismiss="modal" class="btn btn-outline-secondary w-24 mr-1">Cancel</button>
+                                        <button type="submit" class="btn btn-danger w-24">Delete</button>
+                                      </form>
+                                  
+                                  
                                     </div>
                                 </div>
-                                <div class="px-5 pb-8 text-center">
-                               
-                               
-                                  <form action="{{ route('pcategory.destroy', $child->id) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="button" data-tw-dismiss="modal" class="btn btn-outline-secondary w-24 mr-1">Cancel</button>
-                                    <button type="submit" class="btn btn-danger w-24">Delete</button>
-                                  </form>
-                               
-                               
-                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <!-- END: Delete Child Confirmation Modal -->
+                    <!-- END: Delete Child Confirmation Modal -->
+                  @endforeach
+                @endif
+
+                @endforeach
+
 
               <!-- BEGIN: Edit Confirmation Modal -->
               <div class="modal" tabindex="-1" role="dialog" id="editCategoryModal" aria-hidden="true">
